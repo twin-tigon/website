d360d7bf119d95c48c573794d7a106553dfecdbf
Rodrigo García <yo@rodrigogarcia.me>
2021-04-24 17:29:51 +0200

    Use rollup-plugin-html

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,24 @@
+import html from '@web/rollup-plugin-html';
 import { skypackResolver } from '@vinicius73/rollup-plugin-skypack-resolver';
 import { terser } from 'rollup-plugin-terser';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
 import { copy } from '@web/rollup-plugin-copy';
 
-import { writeFile } from 'fs';
-import { promisify } from 'util';
-
-const write = promisify(writeFile);
-
 export default {
-  input: 'src/app.js',
+  plugins: [
+    html({
+      input: 'index.html',
+    }),
+    skypackResolver({ modules: ['lit'] }),
+    minifyHTML(),
+    terser({
+      ecma: 2020,
+      module: true,
+      warnings: true,
+    }),
+    copy({ patterns: ['**/*.{jpg,png,ico,webmanifest}', '*.svg', '404.html'] }),
+  ],
   output: {
     dir: 'dist',
-    format: 'es',
   },
-  plugins: [
-    skypackResolver({ modules: ['lit'] }),
-    terser(),
-    copy({ patterns: ['**/*.{jpg,png,ico,webmanifest}', '*.{html,svg}'] }),
-    {
-      name: 'index-replace',
-      async writeBundle(options, bundle) {
-        const { fileName, source } = bundle['index.html'];
-        const content = source.toString();
-        const newContent = content.replace('./src/app.js', './app.js');
-        await write(`${process.cwd()}/${options.dir}/${fileName}`, newContent);
-      },
-    },
-  ],
 };
