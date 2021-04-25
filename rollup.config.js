05123a29c866ae2c00f607f8dae217f9758f6b93
Rodrigo García <yo@rodrigogarcia.me>
2021-04-25 08:30:49 +0200

    Use jspm

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,33 @@
 import html from '@web/rollup-plugin-html';
-import { skypackResolver } from '@vinicius73/rollup-plugin-skypack-resolver';
 import { terser } from 'rollup-plugin-terser';
 import { copy } from '@web/rollup-plugin-copy';
+
+import { dependencies } from './package-lock.json';
+
+const CDN_HOST = 'https://jspm.dev';
+
+function jspmResolver() {
+  return {
+    name: 'jspm-resolver',
+    async resolveId(id) {
+      if (id in dependencies) {
+        const { version } = dependencies[id];
+        return {
+          id: `${CDN_HOST}/${id}@${version}`,
+          external: true,
+        };
+      }
+      return null;
+    },
+  };
+}
 
 export default {
   plugins: [
     html({
       input: 'index.html',
     }),
-    skypackResolver({ modules: ['lit'] }),
+    jspmResolver(),
     terser({
       ecma: 2020,
       module: true,
