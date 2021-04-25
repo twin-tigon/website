535d11002cd4ba5a40df4194b28f7243bc1cea75
Rodrigo García <yo@rodrigogarcia.me>
2021-04-25 22:22:38 +0200

    Add minify script

---
+++ script/minify.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import globby from 'globby'; // eslint-disable-line
+import { readFile, writeFile } from 'fs/promises';
+import { minify } from 'html-minifier'; // eslint-disable-line
+
+const PATTERNS = ['**/*.html'];
+const OUTPUT_DIR = 'dist';
+
+async function run() {
+  const cwd = `${process.cwd()}/${OUTPUT_DIR}`;
+  const paths = await globby(PATTERNS, { cwd });
+
+  await Promise.all(
+    paths.map(async path => {
+      const outPath = `${cwd}/${path}`;
+      const content = (await readFile(outPath)).toString();
+      const minified = minify(content, {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeOptionalTags: true,
+        removeRedundantAttributes: true,
+        removeTagWhitespace: true,
+        useShortDoctype: true,
+        minifyCSS: true,
+        minifyJS: true,
+      });
+      await writeFile(outPath, minified);
+    }),
+  );
+}
+
+run();
