64c36bdecca3ee2a3f368edafaf38809d84d82a9
Rodrigo García <yo@rodrigogarcia.me>
2021-11-14 09:42:46 +0100

    Inline fonts

--- scripts/minify.js
+++ scripts/minify.js
@@ -1,17 +1,34 @@
 #!/usr/bin/env node
 
 import { readFile, writeFile } from 'fs/promises';
+import fetch from 'node-fetch/lib/index.mjs'; // eslint-disable-line
 import { minify } from 'html-minifier'; // eslint-disable-line
 import rimraf from 'rimraf'; // eslint-disable-line
 
 const HTML_PATH = 'dist/index.html';
 const APP_PATH = 'dist/src/app.js';
+const FONTS_URL =
+  'https://fonts.googleapis.com/css2?family=Bitter:wght@400;700&family=Space+Mono&display=swap';
 
 async function run() {
   const html = (await readFile(HTML_PATH)).toString();
   const app = (await readFile(APP_PATH)).toString();
+  const fonts = await (await fetch(FONTS_URL)).text();
 
-  const minified = minify(html, {
+  const inlinedHtml = html
+    .replace(
+      `<link
+      href="https://fonts.googleapis.com/css2?family=Bitter:wght@400;700&family=Space+Mono&display=swap"
+      rel="stylesheet"
+    />`,
+      `<style>${fonts}</style>`,
+    )
+    .replace(
+      '<script src="src/app.js" type="module"></script>',
+      `<script type="module">${app}</script>`,
+    );
+
+  const minified = minify(inlinedHtml, {
     collapseWhitespace: true,
     removeComments: true,
     removeOptionalTags: true,
@@ -20,10 +37,7 @@ async function run() {
     useShortDoctype: true,
     minifyCSS: true,
     minifyJS: true,
-  }).replace(
-    '<script src="src/app.js"type="module"></script>',
-    `<script type="module">${app}</script>`,
-  );
+  });
 
   await writeFile(HTML_PATH, minified);
 
