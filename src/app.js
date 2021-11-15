97c6705189b88e347dec4e1a55933103ae45db61
Rodrigo García <yo@rodrigogarcia.me>
2021-11-15 20:52:59 +0100

    Move source link to bottom right

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ const STYLES = css`
 
   a {
     position: absolute;
-    left: 0;
+    right: 0;
     bottom: 0;
   }
 
@@ -66,7 +66,7 @@ const STYLES = css`
     }
 
     main {
-      flex-direction: row-reverse;
+      flex-direction: row;
     }
 
     html-renderer {
