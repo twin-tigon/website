5521428961398b219835fa4c0a0a4afce70431b7
Rodrigo García <yo@rodrigogarcia.me>
2021-11-27 16:23:21 +0100

    Remove inlining of css and js

--- scripts/minify.js
+++ scripts/minify.js
@@ -1,34 +1,14 @@
 #!/usr/bin/env node
 
 import { readFile, writeFile } from 'fs/promises';
-import fetch from 'node-fetch';
 import { minify } from 'html-minifier';
-import rimraf from 'rimraf';
 
 const HTML_PATH = 'dist/index.html';
-const APP_PATH = 'dist/src/app.js';
-const FONTS_URL =
-  'https://fonts.googleapis.com/css2?family=Bitter:wght@400;700&family=Space+Mono&display=swap';
 
 async function run() {
   const html = (await readFile(HTML_PATH)).toString();
-  const app = (await readFile(APP_PATH)).toString();
-  const fonts = await (await fetch(FONTS_URL)).text();
 
-  const inlinedHtml = html
-    .replace(
-      `<link
-      href="https://fonts.googleapis.com/css2?family=Bitter:wght@400;700&family=Space+Mono&display=swap"
-      rel="stylesheet"
-    />`,
-      `<style>${fonts}</style>`,
-    )
-    .replace(
-      '<script src="/src/app.js" type="module"></script>',
-      `<script type="module">${app}</script>`,
-    );
-
-  const minified = minify(inlinedHtml, {
+  const minified = minify(html, {
     collapseWhitespace: true,
     removeComments: true,
     removeOptionalTags: true,
@@ -40,8 +20,6 @@ async function run() {
   });
 
   await writeFile(HTML_PATH, minified);
-
-  rimraf.sync('dist/src');
 }
 
 run();
