76049e1e2b9a5cad713ec4b5dfe750deb860d100
Rodrigo García <yo@rodrigogarcia.me>
2021-12-02 13:55:40 +0100

    Add utils tests

---
+++ test/utils.test.js
@@ -0,0 +1,166 @@
+import { expect } from '@open-wc/testing';
+
+import { removeLitComments } from '../src/utils.js';
+import { CONTENT } from '../src/content.js';
+
+const CONTENT_STRING = `<!--?lit$14090330355$-->{
+    "name": "Rodrigo García León",
+    "description": "Full Stack Developer 🙌✨",
+    "projects": [
+        {
+            "name": "hn-app",
+            "description": "Hacker News PWA built with React Hooks",
+            "url": "https://github.com/rodrigo-garcia-leon/hn-app",
+            "keywords": [
+                "frontend",
+                "react",
+                "pwa",
+                "js"
+            ]
+        },
+        {
+            "name": "hn-api",
+            "description": "Hacker News API using GraphQL",
+            "url": "https://github.com/rodrigo-garcia-leon/hn-api",
+            "keywords": [
+                "backend",
+                "koa",
+                "graphql",
+                "nodejs",
+                "js"
+            ]
+        },
+        {
+            "name": "cloud",
+            "description": "Collection of scripts to manage Google Cloud Platform and Kubernetes ☁️",
+            "url": "https://github.com/rodrigo-garcia-leon/cloud",
+            "keywords": [
+                "devops",
+                "gcp",
+                "kubernetes",
+                "shell"
+            ]
+        },
+        {
+            "name": "website",
+            "description": "This website 👀",
+            "url": "https://github.com/rodrigo-garcia-leon/website",
+            "keywords": [
+                "frontend",
+                "lit",
+                "skypack",
+                "esbuild",
+                "webcomponents",
+                "js"
+            ]
+        },
+        {
+            "name": "esbuild-skypack-resolver",
+            "description": "An esbuild plugin to resolve package names to Skypack CDN URLs",
+            "url": "https://github.com/rodrigo-garcia-leon/esbuild-skypack-resolver",
+            "keywords": [
+                "esbuild",
+                "esbuild-plugin",
+                "skypack",
+                "typescript"
+            ]
+        },
+        {
+            "name": "web-hacks",
+            "description": "Web Hacks 🕸🤓",
+            "url": "https://github.com/rodrigo-garcia-leon/web-hacks",
+            "keywords": [
+                "frontend",
+                "houdini",
+                "js"
+            ]
+        },
+        {
+            "name": "glissando-daw",
+            "description": "A web-based digital audio workstation using the web platform APIs (Web Audio, Web MIDI) and WebAssembly",
+            "url": "https://github.com/glissando-daw/glissando-daw",
+            "keywords": [
+                "frontend",
+                "web-audio",
+                "wasm",
+                "rust",
+                "js"
+            ]
+        },
+        {
+            "name": "tictactoe-cli",
+            "description": "Tic-tac-toe implementation in Rust",
+            "url": "https://github.com/rodrigo-garcia-leon/tictactoe-rust",
+            "keywords": [
+                "cli",
+                "gamedev",
+                "rust"
+            ]
+        },
+        {
+            "name": "senescence",
+            "description": "Senescence is a narrative puzzle platformer made for GMTK Game Jam 2020 ",
+            "url": "https://github.com/rodrigo-garcia-leon/senescence",
+            "keywords": [
+                "gamedev",
+                "godot",
+                "game-jam",
+                "puzzle-game",
+                "platform-game"
+            ]
+        },
+        {
+            "name": "thesis-matlab",
+            "description": "Collection of MATLAB scripts and toolboxes regarding my Master Thesis on psychoacoustics",
+            "url": "https://github.com/rodrigo-garcia-leon/thesis-matlab",
+            "keywords": [
+                "matlab",
+                "dsp",
+                "psychoacoustics"
+            ]
+        },
+        {
+            "name": "dotfiles",
+            "description": "My dotfiles",
+            "url": "https://github.com/rodrigo-garcia-leon/dotfiles",
+            "keywords": [
+                "dotfiles",
+                "shell"
+            ]
+        },
+        {
+            "name": "cv",
+            "description": "My CV",
+            "url": "https://github.com/rodrigo-garcia-leon/cv",
+            "keywords": [
+                "cv"
+            ]
+        }
+    ],
+    "contactInfo": [
+        {
+            "name": "Twitter",
+            "value": "https://twitter.com/garcialeonrodri"
+        },
+        {
+            "name": "Instagram",
+            "value": "https://www.instagram.com/rodrigogarcialeon/"
+        },
+        {
+            "name": "LinkedIn",
+            "value": "https://www.linkedin.com/in/rgarcialeon/"
+        },
+        {
+            "name": "Email",
+            "value": "yo@rodrigogarcia.me"
+        }
+    ]
+}`;
+
+describe('utils', () => {
+  describe('removeLitComments', () => {
+    it('ok', () => {
+      expect(JSON.parse(removeLitComments(CONTENT_STRING))).to.deep.equal(CONTENT);
+    });
+  });
+});
