90bb54c97bcd403dde3daf472597f1d8a34ee6e4
Rodrigo García <yo@rodrigogarcia.me>
2024-03-17 17:45:06 -0600

    Fix tests

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,190 +4,135 @@ import { removeLitComments } from '../src/utils.js';
 import { CONTENT } from '../src/content.js';
 
 const CONTENT_STRING = `<!--?lit$14090330355$-->{
-    "name": "Rodrigo García León",
-    "description": "Full Stack Developer 🙌✨",
-    "projects": [
-        {
-            "name": "hn-app",
-            "description": "Hacker News PWA built with React Hooks",
-            "url": "https://github.com/rodrigo-garcia-leon/hn-app",
-            "keywords": [
-                "front-end",
-                "progressive-web-app",
-                "react",
-                "javascript"
-            ]
-        },
-        {
-            "name": "hn-api",
-            "description": "Hacker News API using GraphQL",
-            "url": "https://github.com/rodrigo-garcia-leon/hn-api",
-            "keywords": [
-                "back-end",
-                "koa",
-                "graphql",
-                "nodejs",
-                "javascript"
-            ]
-        },
-        {
-            "name": "cloud",
-            "description": "Collection of scripts to manage Google Cloud Platform and Kubernetes ☁️",
-            "url": "https://github.com/rodrigo-garcia-leon/cloud",
-            "keywords": [
-                "devops",
-                "google-cloud-platform",
-                "kubernetes",
-                "shell"
-            ]
-        },
-        {
-            "name": "website",
-            "description": "This website 👀",
-            "url": "https://github.com/rodrigo-garcia-leon/website",
-            "keywords": [
-                "front-end",
-                "lit",
-                "web-components",
-                "open-wc",
-                "modern-web",
-                "esbuild",
-                "skypack",
-                "javascript"
-            ]
-        },
-        {
-            "name": "esbuild-skypack-resolver",
-            "description": "An esbuild plugin to resolve package names to Skypack CDN URLs",
-            "url": "https://github.com/rodrigo-garcia-leon/esbuild-skypack-resolver",
-            "keywords": [
-                "esbuild",
-                "plugin",
-                "skypack",
-                "typescript"
-            ]
-        },
-        {
-            "name": "web-hacks",
-            "description": "Web Hacks 🕸🤓",
-            "url": "https://github.com/rodrigo-garcia-leon/web-hacks",
-            "keywords": [
-                "front-end",
-                "houdini",
-                "javascript"
-            ]
-        },
-        {
-            "name": "chat-groups-app",
-            "description": "Slack clone app built with React",
-            "url": "https://github.com/rodrigo-garcia-leon/chat-groups-app",
-            "keywords": [
-                "front-end",
-                "react",
-                "web-sockets",
-                "javascript"
-            ]
-        },
-        {
-            "name": "chat-groups-api",
-            "description": "Slack clone API built with Django",
-            "url": "https://github.com/rodrigo-garcia-leon/chat-groups-api",
-            "keywords": [
-                "back-end",
-                "django",
-                "web-sockets",
-                "postgresql",
-                "redis",
-                "docker",
-                "python"
-            ]
-        },
-        {
-            "name": "todos-lists-api",
-            "description": "Todos Lists API using Flask and PyMongo",
-            "url": "https://github.com/rodrigo-garcia-leon/todos-lists-api",
-            "keywords": [
-                "back-end",
-                "flask",
-                "pymongo",
-                "mongodb",
-                "python"
-            ]
-        },
-        {
-            "description": "Authentication API using JWT built with Go and Redis",
-            "keywords": [
-                "back-end",
-                "jwt",
-                "redis",
-                "go"
-            ],
-            "name": "authentication-api",
-            "url": "https://github.com/rodrigo-garcia-leon/authentication-api"
-        },
-        {
-            "name": "glissando-daw",
-            "description": "A web-based digital audio workstation using the web platform APIs (Web Audio, Web MIDI) and WebAssembly",
-            "url": "https://github.com/glissando-daw/glissando-daw",
-            "keywords": [
-                "front-end",
-                "web-audio",
-                "web-assembly",
-                "rust",
-                "javascript"
-            ]
-        },
-        {
-            "name": "tictactoe-cli",
-            "description": "Tic-tac-toe implementation in Rust",
-            "url": "https://github.com/rodrigo-garcia-leon/tictactoe-rust",
-            "keywords": [
-                "cli",
-                "gamedev",
-                "rust"
-            ]
-        },
-        {
-            "name": "senescence",
-            "description": "Senescence is a narrative puzzle platformer made for GMTK Game Jam 2020 ",
-            "url": "https://github.com/rodrigo-garcia-leon/senescence",
-            "keywords": [
-                "gamedev",
-                "game-jam",
-                "platform-game",
-                "puzzle-game",
-                "godot"
-            ]
-        },
-        {
-            "name": "thesis-matlab",
-            "description": "Collection of MATLAB scripts and toolboxes regarding my Master Thesis on psychoacoustics",
-            "url": "https://github.com/rodrigo-garcia-leon/thesis-matlab",
-            "keywords": [
-                "psychoacoustics",
-                "digital-signal-processing",
-                "matlab"
-            ]
-        }
-    ],
-    "contactInfo": [
-        {
-            "name": "Twitter",
-            "value": "https://twitter.com/garcialeonrodri"
-        },
-        {
-            "name": "Instagram",
-            "value": "https://www.instagram.com/rodrigogarcialeon/"
-        },
-        {
-            "name": "LinkedIn",
-            "value": "https://www.linkedin.com/in/rgarcialeon/"
-        },
-        {
-            "name": "Email",
-            "value": "yo@rodrigogarcia.me"
-        }
-    ]
+  "name": "Rodrigo García León",
+  "description": "Full Stack Developer 🙌✨",
+  "projects": [
+    {
+      "name": "website",
+      "description": "This website 👀",
+      "url": "https://github.com/rodrigo-garcia-leon/website",
+      "keywords": [
+        "front-end",
+        "lit",
+        "web-components",
+        "open-wc",
+        "modern-web",
+        "esbuild",
+        "skypack",
+        "javascript"
+      ]
+    },
+    {
+      "name": "esbuild-skypack-resolver",
+      "description": "An esbuild plugin to resolve package names to Skypack CDN URLs",
+      "url": "https://github.com/rodrigo-garcia-leon/esbuild-skypack-resolver",
+      "keywords": ["esbuild", "plugin", "skypack", "typescript"]
+    },
+    {
+      "name": "web-hacks",
+      "description": "Web Hacks 🕸🤓",
+      "url": "https://github.com/rodrigo-garcia-leon/web-hacks",
+      "keywords": ["front-end", "houdini", "javascript"]
+    },
+    {
+      "name": "chat-groups-app",
+      "description": "Slack clone app built with React",
+      "url": "https://github.com/rodrigo-garcia-leon/chat-groups-app",
+      "keywords": ["front-end", "react", "web-sockets", "javascript"]
+    },
+    {
+      "name": "chat-groups-api",
+      "description": "Slack clone API built with Django",
+      "url": "https://github.com/rodrigo-garcia-leon/chat-groups-api",
+      "keywords": [
+        "back-end",
+        "django",
+        "web-sockets",
+        "postgresql",
+        "redis",
+        "docker",
+        "python"
+      ]
+    },
+    {
+      "name": "thesis-matlab",
+      "description": "Collection of MATLAB scripts and toolboxes regarding my Master Thesis on psychoacoustics",
+      "url": "https://github.com/rodrigo-garcia-leon/thesis-matlab",
+      "keywords": ["psychoacoustics", "digital-signal-processing", "matlab"]
+    },
+    {
+      "name": "authentication-api",
+      "description": "Authentication API using JWT built with Go and Redis",
+      "url": "https://github.com/rodrigo-garcia-leon/authentication-api",
+      "keywords": ["back-end", "jwt", "redis", "go"]
+    },
+    {
+      "name": "glissando-daw",
+      "description": "A web-based digital audio workstation using the web platform APIs (Web Audio, Web MIDI) and WebAssembly",
+      "url": "https://github.com/glissando-daw/glissando-daw",
+      "keywords": [
+        "front-end",
+        "web-audio",
+        "web-assembly",
+        "rust",
+        "javascript"
+      ]
+    },
+    {
+      "name": "hn-app",
+      "description": "Hacker News PWA built with React Hooks",
+      "url": "https://github.com/rodrigo-garcia-leon/hn-app",
+      "keywords": ["front-end", "progressive-web-app", "react", "javascript"]
+    },
+    {
+      "name": "hn-api",
+      "description": "Hacker News API using GraphQL",
+      "url": "https://github.com/rodrigo-garcia-leon/hn-api",
+      "keywords": ["back-end", "koa", "graphql", "nodejs", "javascript"]
+    },
+    {
+      "name": "cloud",
+      "description": "Collection of scripts to manage Google Cloud Platform and Kubernetes ☁️",
+      "url": "https://github.com/rodrigo-garcia-leon/cloud",
+      "keywords": ["devops", "google-cloud-platform", "kubernetes", "shell"]
+    },
+    {
+      "name": "tictactoe-cli",
+      "description": "Tic-tac-toe implementation in Rust",
+      "url": "https://github.com/rodrigo-garcia-leon/tictactoe-rust",
+      "keywords": ["cli", "gamedev", "rust"]
+    },
+    {
+      "name": "senescence",
+      "description": "Senescence is a narrative puzzle platformer made for GMTK Game Jam 2020 ",
+      "url": "https://github.com/rodrigo-garcia-leon/senescence",
+      "keywords": [
+        "gamedev",
+        "game-jam",
+        "platform-game",
+        "puzzle-game",
+        "godot"
+      ]
+    }
+  ],
+  "contactInfo": [
+    {
+      "name": "Twitter",
+      "value": "https://twitter.com/garcialeonrodri"
+    },
+    {
+      "name": "Instagram",
+      "value": "https://www.instagram.com/rodrigogarcialeon/"
+    },
+    {
+      "name": "LinkedIn",
+      "value": "https://www.linkedin.com/in/rgarcialeon/"
+    },
+    {
+      "name": "Email",
+      "value": "yo@rodrigogarcia.me"
+    }
+  ]
 }`;
 
 describe('utils', () => {
